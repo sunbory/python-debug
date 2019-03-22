7efaa84a1350817ddd0e0abcbf60054983f5c5c7
屋打头 <sunbo_1@foxmail.com>
2019-03-22 17:22:25 +0800

    Update start.sh

--- rinetd/start.sh
+++ rinetd/start.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-ROOT=$(pwd $(cd $(driname $0)))
+ROOT=$(pwd $(cd $(dirname $0)))
+
+fail() {
+  local msg=$1
+  echo -e "\e[1;31m===> $(date) ERROR: ${msg}\e[0m"
+  exit 1
+}
+
+# test if there is a rinetd instance
+procNum=$(ps afx | grep -v grep | grep './rinetd -c rinetd.conf' | wc -l)
+if [[ procNum -gt 0 ]]; then
+    fail "rinetd have been installed"
+    exit 1
+fi
 
 cd $ROOT && ./rinetd -c rinetd.conf
 
-echo $$ > ./pid
+pid=$(ps afx | grep -v grep | grep './rinetd -c rinetd.conf' | awk '{print $1}')
+
+echo $pid > ./pid

396980f874572cd26f4cb0ab36fb8ab8bc3973ea
屋打头 <sunbo_1@foxmail.com>
2019-03-28 17:59:12 +0800

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-while true; do
+case $1 in
+Start)
+    docker run -d --rm -p 3000:3000 -v $(pwd):/opt/ remote-debugging-docker
+    ;;
+Stop)
+    docker stop $(docker ps | awk '/remote-debugging-docker/{print $1}')
+    ;;
+*)    
+    while true; do
 
-    python -m ptvsd --host 0.0.0.0 --port 3000 --wait -m py
+        python -m ptvsd --host 0.0.0.0 --port 3000 --wait -m py
 
-    sleep 1
+        sleep 1
 
-done
+    done
+    ;;
+esac

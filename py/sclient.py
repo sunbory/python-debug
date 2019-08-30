134d7fe3e7ab0541410868b1ea5392bdea61a2e3
屋打头 <sunbo_1@foxmail.com>
2019-08-30 17:41:42 +0800

    Update sclient.py

--- py/sclient.py
+++ py/sclient.py
@@ -16,21 +16,22 @@ from scp import SCPClient
 
 class SClient(object):
     
-    def __init__(self, host, port, user, password):
+    def __init__(self, host, port, user, password, proxy):
         # paramiko.util.log_to_file(SSH_LOG_PATH)
         self.host=host
         self.port=port
         self.user=user
         self.pwd=password
+        self.proxy = proxy
         self.client = paramiko.SSHClient()
         self.client.load_system_host_keys()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self._connected = False
 
-    def connect(self, use_proxy=None):
+    def connect(self):
         sock = None
-        if isinstance(use_proxy, SClient):
-            sock = use_proxy.open_channel(self)
+        if isinstance(self.proxy, SClient):
+            sock = self.proxy.open_channel(self)
         try:
             self.client.connect(hostname=self.host, port=self.port, username=self.user, password=self.pwd, sock=sock)
             self._connected = True
